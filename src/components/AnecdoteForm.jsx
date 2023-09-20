27ac3dfb986f8ce5195122203751aae1ab7f5596
Adria Huertas <adriahc@gmail.com>
2023-09-20 14:20:54 +0200

    Finished 6.24

--- src/components/AnecdoteForm.jsx
+++ src/components/AnecdoteForm.jsx
@@ -4,8 +4,8 @@ import { useNotificationDispatch } from "../context/NotificationContext"
 
 const AnecdoteForm = () => {
   const notificationDispatch = useNotificationDispatch()
-  
-  let timeoutId;
+
+  let timeoutId
 
   const queryClient = useQueryClient()
 
@@ -13,22 +13,32 @@ const AnecdoteForm = () => {
     onSuccess: (newAnecdote) => {
       const anecdotes = queryClient.getQueryData("anecdotes")
       queryClient.setQueryData("anecdotes", anecdotes.concat(newAnecdote))
-    }
+    },
   })
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-    newAnecdoteMutation.mutate({content, votes: 0})
-    notificationDispatch({type: 'SET_NOTIFICATION', data: `you created '${content}'`})
-}
+    event.target.anecdote.value = ""
+    console.log("new anecdote")
+    if (content.length < 5) {
+      notificationDispatch({
+        type: "SET_NOTIFICATION",
+        data: "too short anecdote, must have length 5 or more",
+      })
+    } else {
+      newAnecdoteMutation.mutate({ content, votes: 0 })
+      notificationDispatch({
+        type: "SET_NOTIFICATION",
+        data: `you created '${content}'`,
+      })
+    }
+  }
 
   return (
     <div>
       <h3>create new</h3>
       <form onSubmit={onCreate}>
-        <input name='anecdote' />
+        <input name="anecdote" />
         <button type="submit">create</button>
       </form>
     </div>
